b14771d5dace8b4073203f1f808b578f20ccc625
David DeBoer <ddeboer@berkeley.edu>
2018-06-08 16:20:10 -0700

    updated ridzen and added zen ridz file

--- ridzen.py
+++ ridzen.py
@@ -7,13 +7,17 @@ import numpy as np
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-d', '--day_to_use', help="If just do one day.", default=None)
-ap.add_argument('-x', '--dontwrite', help="Flag to turn off file writing", action="store_true")
+ap.add_argument('-p', '--path_to_save', help="Path for output files. Will create if doesn't exist.", default='data_ridzen')
+ap.add_argument('-X', '--dontwrite', help="Flag to turn off file writing", action="store_true")
+ap.add_argument('-n', '--num_chan', help="Number of channels.", default=1024)
 args = ap.parse_args()
+
+if not os.path.exists(args.path_to_save):
+    os.mkdir(args.path_to_save)
 
 if __name__ == '__main__':
     available_files = sorted(os.listdir('.'))
-    num_chan = 1024
-    freq = (250.0 / num_chan) * np.arange(num_chan)
+    freq = (250.0 / args.num_chan) * np.arange(num_chan)
 
     for af in available_files:
         ident = af.split('.')[0]
@@ -30,14 +34,15 @@ if __name__ == '__main__':
         for a, v in df.iteritems():
             if a == 'times':
                 continue
-            if len(v) != num_chan:
+            if len(v) != args.num_chan:
                 right_num_chan = False
                 print("Channel number is {}, expected {}".format(len(v), num_chan))
                 break
             c += v
         if not args.dontwrite and right_num_chan:
             c /= len(df['times'])
-            nfn = "dataout/{}.{}.val.I".format(ident, time_stamp)
+            nfn = "{}.{}.val.I".format(ident, time_stamp)
+            nfn = os.path.join(args.path_to_save, nfn)
             with open(nfn, 'w') as fp:
                 for fc, cc in zip(freq, c):
                     fp.write("{} {}\n".format(fc, cc))
