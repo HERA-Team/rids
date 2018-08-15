2973ad44c865546fe9563503e5d4f0d733cbc5a7
David DeBoer <ddeboer@berkeley.edu>
2018-08-15 15:59:42 -0700

    added zip/unzip script

---
+++ scripts/zipr.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+# _*_ coding: utf-8 _*_
+# Copy 2018 the HERA Project
+# Licensed under the 2-clause BSD license
+from __future__ import print_function, division, absolute_import
+
+import argparse
+import gzip
+
+ap = argparse.ArgumentParser()
+ap.add_argument('file', help="file(s) to use", default=None)
+ap.add_argument('-d', '--delete', help="Set flag to delete other version of file.", action='store_true')
+
+args = ap.parse_args()
+
+
+if __name__ == '__main__':
+    file_type = args.file.split('.')[-1]
+    if file_type == 'ridz':
+        out_filename = '.'.join(args.file.split('.')[:-1]) + '.rids'
+        infp = gzip.GzipFile(args.file, 'rb')
+        outfp = file(out_filename, 'wb')
+    else:
+        out_filename = '.'.join(args.file.split('.')[:-1]) + '.ridz'
+        infp = file(args.file, 'rb')
+        outfp = gzip.GzipFile(out_filename, 'wb')
+
+    s = infp.read()
+    infp.close()
+    outfp.write(s)
+    outfp.close()
+
+    if args.delete:
+        import os
+        os.remove(args.file)
