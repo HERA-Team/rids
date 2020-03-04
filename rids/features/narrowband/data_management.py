24dba166bb32c3a2bca18c379b842b9c4d03c82d
Bobby Pascua <r.pascua@berkeley.edu>
2020-03-03 18:12:18 -0800

    Added a function for extracting a file glob given some parameters used to identify the desired files. Has not yet been tested in full, but it *should* work as intended...

--- rids/features/narrowband/data_management.py
+++ rids/features/narrowband/data_management.py
@@ -3,6 +3,10 @@
 # Licensed under the 2-clause BSD license.
 
 # update imports as needed; XXX remove this comment before merging to master
+import glob
+import os
+import re
+
 import numpy as np
 from astropy import constants, units
 
@@ -11,9 +15,117 @@ import uvtools
 import pyuvdata
 from pyuvdata import UVData
 
-def reduce_data(uvd, use_ants=None, use_bls=None, use_pols='linear',
-                use_autos=True, use_cross=False, use_times=None,
-                use_freqs=None):
+def construct_file_glob(obs_season=None, jd=None, filetype='uvh5',
+                        dir_path=None, file_re=None):
+    """
+    Make a glob of visibility files given an observing season and JD.
+
+
+    This function takes an observing season and a Julian Date, or a 
+    path to a directory containing visibility files, and makes a sorted 
+    glob of all of the visibility files pertaining to the specified 
+    parameters. Specifying ``dir_path`` and ``file_re`` offers the 
+    greatest control over which files are loaded in and may completely 
+    specify which files to extract; not specifying these parameters 
+    results in the function making these decisions, based on the 
+    structure of the file storage on lustre at the time of writing.
+    Note that specifying ``dir_path`` and ``file_re`` *should* allow 
+    you to use this function on a local machine, or on a different 
+    filepath on whichever computing cluster you are using.
+
+    Parameters
+    ----------
+    obs_season : str
+        Observing season; currently supported options are 'H1C', 'H2C', 
+        and 'H3C'. If specifying 'H1C', then you must append the appropriate 
+        IDR tag ('_IDR1', '_IDR2', '_IDR3') to ``obs_season`` before 
+        passing the parameter to this function. This parameter does not 
+        need to be specified if ``dir_path`` is specified.
+
+    jd : int or str
+        Julian Date for which to extract observational data. This must be 
+        specified if ``obs_season`` is also specified. If ``dir_path`` and 
+        ``file_re`` are specified, then this is ignored.
+
+    filetype : str, optional
+        Extension used for the visibility files; must be a type supported 
+        by ``pyuvdata``. Default is 'uvh5'.
+
+    dir_path : str, optional
+        Path to the directory containing the visibility files, may be 
+        absolute or relative (use relative paths at your own risk). Must 
+        be specified if ``obs_season`` and ``jd`` are not specified.
+
+    file_re : str or re.Pattern
+        String to construct a regular expression for identifying files, 
+        or a compiled re.Pattern object. Ideally, this is specified if 
+        ``dir_path`` is specified; however, ``dir_path`` and ``jd`` 
+        together (perhaps with ``filetype``) should suffice if the file 
+        naming convention follows 'zen.JD.HH.uvh5'.
+
+    Returns
+    -------
+    vis_file_glob : list of str
+        Sorted list of visibility files.
+
+    Raises
+    ------
+    FileNotFoundError
+        Raised if ``vis_file_glob`` is empty.
+    """
+    # XXX rewrite this at a later date to use the Librarian?
+    insufficient_info_err_msg = \
+        "Not enough information to construct the file glob. Please " \
+        "refer to the documentation for this function to determine " \
+        "which parameters must be specified."
+    if dir_path is None:
+        if obs_season is None or jd is None:
+            raise ValueError(insufficient_info_err_msg)
+
+        dir_path = "/lustre/aoc/projects/hera/{obs_season}/{jd}"
+        if obs_season == "H1C_IDR3":
+            obs_season = "H1C_IDR3/IDR3_1"
+        dir_path.format(obs_season=obs_season, jd=jd)
+        
+        # sometimes things are nested... see some H3C directories
+        if glob.glob(os.path.join(dir_path, "zen.*")) == []:
+            dir_path = os.path.join(dir_path, "{jd}").format(jd=jd)
+
+    if file_re is None:
+        # make sure we have enough information to find the files
+        if jd is None:
+            raise ValueError(insufficient_info_err_msg)
+        file_prefix = "zen.{jd}".format(jd=jd)
+        # update this if we go back to including HH in file names
+        if int(jd) >= 2458838:
+            file_re = re.compile(
+                file_prefix + ".[0-9]{5}." + filetype
+            )
+        else:
+            file_re = re.compile(
+                file_prefix + ".[0-9]{5}.HH." + filetype
+            )
+
+    # this is kind of lazy, but whatever
+    vis_file_glob = glob.glob(dir_path)
+    vis_file_glob = sorted(
+        [
+            vis_file for vis_file in vis_file_glob 
+            if file_re.findall(vis_file) != []
+        ]
+    )
+
+    if vis_file_glob == []:
+        raise FileNotFoundError(
+            "No visibility files were found. Please check your settings."
+        )
+
+    return vis_file_glob
+
+
+def uvd_downselect(uvd, use_ants=None, use_bls=None, use_pols='linear',
+                   use_autos=True, use_cross=False, use_times=None,
+                   use_freqs=None):
     """Select only a subset of the data in ``uvd``.
 
     XXX refer to numpydoc style (does UVData etc receive ``...``?)
